77e5fc699852668ae48884c389396f1ad5fb6885
igor023 <thedarthdot@gmail.com>
2025-07-09 12:00:11 -0400

    c

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,49 +1,110 @@
-#include <fmt/core.h>
-#include "google/cloud/speech/v2/speech_client.h"
-#include "google/cloud/project.h"
+#include <portaudio.h>
+#include "google/cloud/speech/v1/speech_client.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include "google/cloud/speech/v1/speech_client.h"
+#include <cstring>
 
-void ConfigureRecognizer(google::cloud::speech::v2::RecognizeRequest& request) {
-  *request.mutable_config()->add_language_codes() = "en-US";
-  request.mutable_config()->set_model("short");
-  *request.mutable_config()->mutable_auto_decoding_config() = {};
+namespace speech = ::google::cloud::speech::v1;
+namespace speech_v1 = google::cloud::speech_v1::v2_37;
+
+
+std::string ReadFileToString(const std::string& file_path) {
+	std::ifstream file(file_path, std::ios::binary);
+	std::ostringstream contents;
+	contents << file.rdbuf();
+	return contents.str();
 }
 
+static void checkPaErr(PaError err) {
+	if(err != paNoError) {
+		printf("PortAudio error: %s\n", Pa_GetErrorText(err));
+		exit(EXIT_FAILURE);
+	}
+}
 
-int main(int argc, char* argv[]) try {
-  auto constexpr kDefaultUri = "gs://cloud-samples-data/speech/hello.wav";
-  if (argc != 3 && argc != 4) {
-    std::cerr << "Usage: " << argv[0] << " project <region>|global [gcs-uri]\n"
-              << "  Specify the region desired or \"global\"\n"
-              << "  The gcs-uri must be in gs://... format. It defaults to "
-              << kDefaultUri << "\n";
-    return 1;
-  }
-  std::string const project = argv[1];
-  std::string location = argv[2];
-  auto const uri = std::string{argc == 4 ? argv[3] : kDefaultUri};
-  namespace speech = ::google::cloud::speech_v2;
+void showDevices() {
+	int numDev = Pa_GetDeviceCount();
+	printf("Num devices: %d\n", numDev);
+	if(numDev < 0) {
+		printf("Error counting devices");
+		exit(EXIT_FAILURE);
+	} else if(numDev == 0) {
+		printf("No devices found");
+		exit(EXIT_SUCCESS);
+	}
 
-  std::shared_ptr<speech::SpeechConnection> connection;
-  google::cloud::speech::v2::RecognizeRequest request;
-  ConfigureRecognizer(request);
-  request.set_uri(uri);
-  request.set_recognizer("projects/" + project + "/locations/" + location +
-                         "/recognizers/_");
+	const PaDeviceInfo* devInfo;
+	for(int i = 0; i < numDev; ++i) {
+		devInfo = Pa_GetDeviceInfo(i);
+		printf("Device %d:\nName: %s\ndefaultSampleRate: %f\n",
+				i,
+				devInfo->name,
+				devInfo->defaultSampleRate);
+	}
 
-  if (location == "global") {
-    // An empty location string indicates that the global endpoint of the
-    // service should be used.
-    location = "";
-  }
+	int dev;
+	cout << "enter dev: ";
+	cin >> dev;
 
-  auto client = speech::SpeechClient(speech::MakeSpeechConnection(location));
-  auto response = client.Recognize(request);
-  if (!response) throw std::move(response).status();
-  std::cout << response->DebugString() << "\n";
+	PaStreamParameters inputParameters;
+	PaStreamParameters outputParameters;
 
-  return 0;
-} catch (google::cloud::Status const& status) {
-  std::cerr << "google::cloud::Status thrown: " << status << "\n";
-  return 1;
+	memset(&inputParameters, 0, sizeOf(inputParameters);
+	inputParameters.channelCount = 2;
+	inputParameters.device = dev;
+	inputParameters.hostApiSpecificStreamInfo = NULL;
+	inputParameters.sampleFormat = paFloat32;
+	inputParameters.suggestedLatency = Pa_GetDeviceInfo(device)->defaultLowInputLatency;
+
+	memset(&outputParameters, 0, sizeof(outputParameters));
+	outputParameters.channelCount = 2;
+	outputParameters.device = dev;
+	outputParameters.hostApiSpecificStreamInfo = NULL;
+	
+}
+
+int main() {
+	PaError err;
+	checkPaErr(Pa_Initialize());
+	showDevices();
+	checkPaErr(Pa_Terminate());
+
+
+	auto client = speech_v1::SpeechClient(speech_v1::MakeSpeechConnection());
+
+	speech::RecognitionConfig config;
+	config.set_encoding(speech::RecognitionConfig::MP3);
+	config.set_sample_rate_hertz(44100);
+	config.set_language_code("en-US");
+
+	auto* diarization_config = config.mutable_diarization_config();
+	diarization_config->set_enable_speaker_diarization(true);
+	diarization_config->set_min_speaker_count(2);
+	diarization_config->set_max_speaker_count(6);
+	
+
+	speech::RecognitionAudio audio;
+	std::string audio_content = ReadFileToString("greetings2.mp3");
+	//audio.set_uri("https://dn721802.ca.archive.org/0/items/englishaudio/10-%20Learn%20English%20Speaking%20%20Easy%20English%20Conversations%2002.mp3");
+	audio.set_content(audio_content);
+	auto response = client.Recognize(config, audio);
+	if (!response) {
+		std::cerr << "Recognition failed: " << response.status() << "\n";
+		return 1;
+	}
+
+	for (auto const& result : response->results()) {
+		auto const& alt = result.alternatives(0);
+		std::cout << "Transcript: " << alt.transcript() << "\n";
+		for (auto const& word : alt.words()) {
+			std::cout << word.word() << " (Speaker " << word.speaker_tag() << ")\n";
+		}
+	}
+
+	return 0;
 }
 
